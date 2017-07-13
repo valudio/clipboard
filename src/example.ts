1db6752d65f13ba6d3393265a4b07b502ba1c1e0
Roberto Huertas <roberto.huertas@outlook.com>
2017-07-13 21:01:10 +0200

    fixed timeout issue

--- src/example.ts
+++ src/example.ts
@@ -5,16 +5,26 @@ setInterval(() => {
     try {
         const before = clipboard.getFromClipboard();
         console.log('Before copy:', before);
-        clipboard.copy();
-        setTimeout(() => {
-            console.log('After copy:', clipboard.getFromClipboard());
-            clipboard.setToClipboard(before);
+
+        // clipboard.copy();
+        // setTimeout(() => {
+        //     console.log('After copy:', clipboard.getFromClipboard());
+        //     clipboard.setToClipboard(before);
+        //     console.log('After setting:', clipboard.getFromClipboard());
+        // }, 150);
+
+        clipboard.getSelectedTextAsync().then(s => {
+            console.log('After copy:', s);
             console.log('After setting:', clipboard.getFromClipboard());
-        }, 150);
-        // clipboard.getSelectedTextAsync().then(s => {
-        //     console.log('After copy:', s);
-        // });
+        });
     } catch (error) {
         console.log(error);
     }
 }, 5000);
+
+// setInterval(async () => {
+//     console.log('Before selection:', clipboard.getFromClipboard());
+//     const s = await clipboard.getSelectedTextAsync();
+//     console.log('After copy:', s);
+//     console.log('After setting:', clipboard.getFromClipboard());
+// }, 5000);
--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ cb.getSelectedTextAsync = () => {
   return new Promise<string>(resolve => {
     const prevValue = cb.getFromClipboard();
     cb.copy();
-    setImmediate(() => {
+    setTimeout(() => {
       const fromCb = cb.getFromClipboard();
       cb.setToClipboard(prevValue);
       resolve(fromCb);
-    });
+    }, 150);
   });
 };
 export const clipboard = cb;
